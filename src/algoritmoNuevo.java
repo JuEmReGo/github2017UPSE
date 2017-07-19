bc5ae77dca52c1dd6a1619feb15c4733d8073058
jerg <emmanuel20071988@gmail.com>
2017-07-19 11:16:31 -0500

    agregar un tercer commit

--- src/algoritmoNuevo.java
+++ src/algoritmoNuevo.java
@@ -1,9 +1,37 @@
+import javax.sound.midi.Synthesizer;
 
 public class algoritmoNuevo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Algoritmo SAlvaje");
+		
+		int i=0;
+		int nota = 0;
+		
+		boolean copiaExamen = true;
+		
+		
+		do{ 
+		 System.out.println("tu nota es:"+ nota);
+		 if(i>5){
+			 break;
+			 		 
+		 }
+			i++;
+			
+		}while(copiaExamen);
+		
+			
 
+	}
+	
+	public static void imprimirTesis()
+	{
+		System.out.println("blabla relleno!! ");
+		System.out.println("mas lata");
+		System.out.println("lata ++");
+	
 	}
 
 }
